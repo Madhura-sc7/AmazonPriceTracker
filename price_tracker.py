fb891822892fbd1e8944373e58f3fae69a46270a
Madhura sc <scmadhura@gmail.com>
2026-07-14 11:12:56 +0530

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -55,6 +55,8 @@ def give_product_price(URL):
 
 result_file = open('my_result_file.text','w')
 
+print(headers)
+
 try:
     for every_product in products_to_track:
         product_price_returned = give_product_price(every_product.get("product_url"))
